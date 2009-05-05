2c3085f48a234ceb1619c8dbabf8e1a5a5066d7e
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-05-04 17:46:24 -0700

    Posting measurements

--- main.py
+++ main.py
@@ -16,22 +16,45 @@ class MainPage(webapp.RequestHandler):
         
 class ActionPage(webapp.RequestHandler):
   def get(self, key, path):
-    ns, value = self.getParameters(path)
-    logging.info('get: %s, %s' % (ns, value))
+    ns, value, v_type = self.getParameters(path)
+    logging.info('get: %s, %s, %s' % (ns, value, v_type))
     self.post(key, path)
 
   def post(self, key, path):
-    ns, value = self.getParameters(path)
-    logging.info('post: %s, %s' % (ns, value))
+    if (not key):
+      self.error(500)
+    campaign = Campaign.get(key)
+    if (not campaign):
+      self.error(404)
+      
+    ns, value, v_type = self.getParameters(path)
+    logging.info('post: %s, %s, %s' % (ns, value, v_type))
+    
+    self.measure(campaign, ns, value, v_type)
+    
+  def measure(self, campaign, ns, value, v_type):
+    datum = Storage(
+      namespace = ns,
+      value = value,
+      type = v_type,
+      campaign = campaign
+    )
+    if (not datum.put()):
+      logging.error('Datum not saved. Campaign: %s %s %s %s' % (campaign, ns, value, v_type))
+      self.error(500)
+    else:
+      self.response.set_status(201)
   
   def getParameters(self, path):
     ns, value = util.getParts(path)
+    v_type = self.request.get('type')
     if (not ns):
       self.error(500)
     if (not value and value is not 0):
-      if (not self.request.get('type')):
+      if (not v_type): # Todo, default_values_dict
         value = 1
-    return (ns, value)
+        v_type = 'number'
+    return (ns, value, v_type)
     
     
 application = webapp.WSGIApplication(debug = os.environ['SERVER_SOFTWARE'].startswith('Dev'), url_mapping = [
