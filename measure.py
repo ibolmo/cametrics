04e43c795bbffb8f8911b7d68a55199a41b2e330
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-05-28 10:25:27 -0700

    Stash-merge

--- measure.py
+++ measure.py
@@ -3,13 +3,22 @@ import os, logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-from myapp.models import Storage, Statistics
-import util, myapp.renderer
+from django.utils import simplejson
+from myapp.models import *
+import util, myapp.renderer as renderer
 
  
 class MainPage(webapp.RequestHandler):
-  def get(self, key, namespace, format):
-    logging.warning("%s, %s, %s" % (key, namespace, format))
+  def get(self, key, path, format):
+    if (not key):
+      self.error(500)
+    
+    campaign = Campaign.all(keys_only = True).filter('__key__ = ', key).get()
+    if (not campaign):
+      self.error(404)
+    
+    ns = (path or '').strip('/').replace('/', '.')
+    logging.warning("%s, %s, %s" % (key, ns, format))
     
     format = format or self.request.GET.get('format', 'json')
     ns, data_path = util.getParts(ns)
@@ -17,10 +26,37 @@ class MainPage(webapp.RequestHandler):
     stats = Statistics.get_by_campaign_and_namespace(campaign, ns)
     return renderer.get(format)(request, format, data, stats, data_path)
     
-  def post(self, key, namespace, format):
-    pass
+  def post(self, key, path, format):
+    if (not key):
+      self.error(500)
     
-
+    campaign = Campaign.all(keys_only = True).filter('__key__ = ', key).get()
+    if (not campaign):
+      self.error(404)
+    
+    ns = (path or '').strip('/').replace('/', '.')
+    logging.warning("%s, %s, %s" % (key, ns, format))
+    
+    if 'bulk' in self.request.get('type'):
+      data = simplejson.loads(self.request.get('data') or '[]')
+      saved = False
+      for datum in data:
+        if create_datum(campaign, datum.get('namespace').strip('/').replace('/', '.'), datum):
+          saved = True
+    else:
+      saved = create_datum(campaign, ns, request.POST)
+    self.set_status(saved and 201 or 304)
+    
+def create_datum(campaign, ns, obj = {}):
+  value = obj.get('value')
+  kind = obj.get('type', 'number')
+  logging.debug('POST | value: %s | kind: %s' % (value, kind))
+  
+  datum = Storage(campaign = campaign, namespace = ns, type = kind, value = value)
+  saved = bool(datum.put())
+  if not saved:
+    logging.critical('Could not save: Storage(%s, %s, %s, %s)' % (campaign, ns, value, kind))
+  return saved
 '''
 def measurements(request, key, path, format):
   logging.debug('measurements::path = %s' % path)
@@ -56,17 +92,6 @@ def measurements(request, key, path, format):
     return HttpResponse('Not yet supported, please contact admin.', status = 304)
 
   return HttpResponse('Internal Error', status = 500)
-
-def create_datum(campaign, ns, obj = {}):
-  value = obj.get('value')
-  kind = obj.get('type', 'number')
-  logging.debug('POST | value: %s | kind: %s' % (value, kind))
-  
-  datum = Storage(campaign = campaign, namespace = ns, type = kind, value = value)
-  saved = bool(datum.put())
-  if not saved:
-    logging.critical('Could not save: Storage(%s, %s, %s, %s)' % (campaign, ns, value, kind))
-  return saved
 
 '''
     
